86983c83c198161c8d5fdcfc74d1bae693c9631f
Ivaylo Petrov <i.petrow@live.com>
2026-07-11 20:44:42 +0300

    add prometheus instrumentation

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_caching import Cache
+from prometheus_flask_exporter import PrometheusMetrics
 
 cache = Cache()
 
@@ -8,10 +9,12 @@ def create_app():
 
     app.config['CACHE_TYPE'] = 'SimpleCache'
     app.config['CACHE_DEFAULT_TIMEOUT'] = 300 
-
     cache.init_app(app)
-
     app.cache = cache
+
+    metrics = PrometheusMetrics(app)
+    metrics.info('ci_dashboard_info', 'CI Dashboard application info', version='1.0.0')
+
 
     from .routes import main
     app.register_blueprint(main)
